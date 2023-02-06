d98f8fefbcb57752a64bada4adc25b31dd5f290d
frank <frankmonli.dalauta@zdspgc.edu.ph>
2023-02-06 14:10:02 +0800

    name

---
+++ hi.py
@@ -0,0 +1,4 @@
+first_name= input("enter your first name: ")
+midlle_name= input("enter your midlle name: ")
+last_name= input("enter your last name: ")
+print("your full name is",first_name,midlle_name,last_name)
